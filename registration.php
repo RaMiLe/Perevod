cc6970c1d46aa3359ba5f048e48b0a6612f7bb95
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-05 01:26:52 +0400

    Update registration.php

--- registration.php
+++ registration.php
@@ -34,13 +34,4 @@
 </form>
 
 <br>Поля со значком <font color="red">*</font> обязательны для заполнения
-<?php
-try {
-    $conn = new PDO("sqlsrv:server = tcp:asus20.database.windows.net,1433; Database = Deneg", "asus97", "Rosbank20");
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-include_once("createtable.php");
 
-if (isset($_POST['submit'])){
-    if(empty($_POST['login']))  {
-    echo '<br><font color="red"><img border="0" src="error.gif" alt="Введите логин"> Введите логин!</font>';
-} 
