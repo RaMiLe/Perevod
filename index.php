a75f9cc5a7298b0d2619c71b4441817b45c0c2ec
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-05 00:05:42 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<Title>Registration Form</Title>
+<style>
+   .blok-center {
+    position: absolute; /* Абсолютное позиционирование */
+    width: 600px; /* Ширина блока */
+    height: 400px; /* Высота блока */
+    margin: auto; /* Отступ от блока */
+    top: 0; /* Положение блока от верхнего края */
+    bottom: 0; /* Положение блока от нижнего края */
+    left: 0; /* Положение блока от левого края */
+    right: 0; /* Положение блока от правого края */
+   
+    border: 1px solid #000; /* Рамка блока */
+    padding: 10px; /* Отступ внутри блока */
+    overflow: auto; /* Полоса прокрутки */
+   }
+</style>
+</head>
+<body bgcolor="#000" background="https://s1.1zoom.ru/big0/81/Russia_Mountains_Lake_469922.jpg">
+	
+<div class="blok-center">
+<h1>Register here!</h1>
+<p>Fill in your name and
+email address, then click <strong>Submit</strong>
+to register.</p>
+
+ <form method="post" action="index.php" enctype="multipart/form-data" >
+<input type ="text" name ="name" id ="name" placeholder ="Введите ваше имя">
+<input type ="text" name ="email" id ="email" placeholder ="Ваш еmail..">
+<input type ="submit" name ="submit" value ="Войти">
+<a href="/C:/Users/user/Desktop/Новая%20папка/перевод.html"> Регистрация </a>
+ 
+</body>
+</form> 
