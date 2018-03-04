ebe26ee3d9826f7803cf4941151a748b611de265
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-05 02:44:11 +0400

    Update registration.php

--- registration.php
+++ registration.php
@@ -29,7 +29,6 @@ to register.</p>
 <form method="post" action="index.php" enctype="multipart/form-data" >
 <input type ="text" name ="name" id ="name" placeholder ="Введите ваше имя">
 <input type ="text" name ="email" id ="email" placeholder ="Ваш еmail..">
-  <input type ="text" name ="Login" id ="Login" placeholder ="Ваш логин..">
 <select name="country">
 <option value="">All</option>
 <option value="Russia">Russia</option>
