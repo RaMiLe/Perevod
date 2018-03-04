1e6bc3e3ed15b823b61681a993e9e5778d9ff126
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-05 01:24:31 +0400

    Update registration.php

--- registration.php
+++ registration.php
@@ -38,7 +38,7 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:asus20.database.windows.net,1433; Database = Deneg", "asus97", "Rosbank20");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-include_once("create.php");
+include_once("createtable.php");
 
 if (isset($_POST['submit'])){
     if(empty($_POST['login']))  {
