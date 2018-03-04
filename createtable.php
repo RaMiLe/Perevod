532536e6e37ede2ddbaf0b72649ad3a6ee312a3c
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-05 00:01:00 +0400

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,1 +1,22 @@
-
+<?php
+// DB connection info
+$host = "localhost\sqlexpress";
+$user = "RaMiLe;
+$pwd = Rosbank1997;
+$db = Ramile;
+try{
+    $conn = new PDO("sqlsrv:server = tcp:asus20.database.windows.net,1433; Database = Deneg", "asus97", "Rosbank20");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE registration_tbl(
+    id INT NOT NULL IDENTITY(1,1) 
+    PRIMARY KEY(id),
+    name VARCHAR(30),
+    email VARCHAR(30),
+    date DATE)";
+    $conn->query($sql);
+}
+catch(Exception $e){
+    die(print_r($e));
+}
+echo "<h3>Table created.</h3>";
+?>
