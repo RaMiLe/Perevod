a08014eaf3c7d06e6a56b66a585982ad693be4dc
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-05 02:32:46 +0400

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,5 +1,3 @@
-<form action="verification.php" method="POST">
-
 <table>
 <tr>
 <td>Логин<font color="red">*</font>:</td>
@@ -34,53 +32,10 @@
 </form>
 
 <br>Поля со значком <font color="red">*</font> обязательны для заполнения
-<html>
-<head>
-<Title>Registration Form</Title>
-<style type="text/css">
-body { background-color:
-#fff; border-top: solid 10px #000;
-color: #333; font-size: .85em;
-margin: 20; padding: 20;
-font-family: "Segoe UI",
-Verdana, Helvetica, Sans-Serif;
-}
-h1, h2, h3,{ color: #000;
-margin-bottom: 0; padding-bottom: 0; }
-h1 { font-size: 2em; }
-h2 { font-size: 1.75em; }
-h3 { font-size: 1.2em; }
-table { margin-top: 0.75em; }
-th { font-size: 1.2em;
-text-align: left; border: none; padding-left: 0; }
-td { padding: 0.25em 2em 0.25em 0em;
-border: 0 none; }
-</style>
-</head>
-<body>
-<h1>Register here!</h1>
-<p>Fill in your name and
-email address, then click <strong>Submit</strong>
-to register.</p>
-<form method="post" action="registration.php" enctype="multipart/form-data" >
-<input type ="text" name ="name" id ="name" placeholder ="Введите ваше имя">
-<input type ="text" name ="email" id ="email" placeholder ="Ваш еmail..">
-<select name="country">
-<option value="">All</option>
-<option value="Russia">Russia</option>
-<option value="USA">USA</option>
-<option value="Germany">Germany</option>
-<option value="Japan">Japan</option>
-<option value="China">China</option>
-</select>
-<input type ="submit" name ="submit" value ="Отправить">
-<input type="submit" name="filter" value="Фильтр">
-</form>
-
 <?php
-$dsn = "sqlsrv:server = tcp:asus20.database.windows.net,1433; Database = deneg";
-$username = "asus97";
-$password = "Rosbank20";
+$dsn = "sqlsrv:server = tcp:rom.database.windows.net,1433; Database = qqq";
+$username = "rom";
+$password = "Rosbank1997";
 try {
 $conn = new PDO($dsn, $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -89,66 +44,56 @@ catch (PDOException $e) {
 print("Ошибка подключения к SQL Server.");
 die(print_r($e));
 }
-
-include_once("bd.php");
-
-if (isset($_POST['submit'])){
-    if(empty($_POST['login']))  {
-    echo '<br><font color="red"><img border="0" src="error.gif" alt="Введите логин"> Введите логин!</font>';
-}
-    if (isset($_POST['submit'])){
-    if(empty($_POST['password']))  {
-    echo '<br><font color="red"><img border="0" src="error.gif" alt="Введите"> Введите пароль!</font>';
-}
-
-elseif(empty($_POST['password'])) {
-echo '<br><font color="red"><img border="0" src="error.gif" alt="Введите пароль !">Введите пароль!</font>';
-}
-elseif (!preg_match("/\A(\w){6,20}\Z/", $_POST['password'])) {
-echo '<br><font color="red"><img border="0" src="error.gif" alt="Пароль слишком короткий!">Пароль слишком короткий! Пароль должен быть не менее 6 символов! </font>';
-}
-elseif(empty($_POST['password2'])) {
-echo '<br><font color="red"><img border="0" src="error.gif" alt="Введите подтверждение пароля!">Введите подтверждение пароля!</font>';
-}
-elseif($_POST['password'] != $_POST['password2']) {
-echo '<br><font color="red"><img border="0" src="error.gif" alt="Введенные пароли не совпадают!">Введенные пароли не совпадают!</font>';
-}
-elseif(empty($_POST['email'])) {
-echo '<br><font color="red"><img border="0" src="error.gif" alt="Введите E-mail!">Введите E-mail! </font>';
-}
-elseif (!preg_match("/^[a-zA-Z0-9_\.\-]+@([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,6}$/", $_POST['email'])) {
-echo '<br><font color="red"><img border="0" src="error.gif" alt="E-mail имеет недопустимий формат!">E-mail имеет недопустимий формат! Например, name@gmail.com! </font>';
-}
-
-else{
-$login = $_POST['login'];
-$password = $_POST['password'];
-$mdPassword = md5($password);
-$password2 = $_POST['password2'];
-$email = $_POST['email'];
-$rdate = date("d-m-Y в H:i");
+if(!empty($_POST)) {
+try {
 $name = $_POST['name'];
-$lastname = $_POST['lastname']; 
-
-$query = ("SELECT id FROM users WHERE login='$login'");
-$sql = mysql_query($query) or die(mysql_error());
-
-if (mysql_num_rows($sql) > 0) {
-echo '<font color="red"><img border="0" src="error.gif" alt="Пользователь с таким логином зарегистрированый!">Пользователь с таким логином зарегистрирован!</font>';
+$email = $_POST['email'];
+$date = date("Y-m-d");
+$country = $_POST['country'];
+if ($name == "" || $email == "") {
+echo "<h3>Не заполнены поля name и email.</h3>";
 }
 else {
-$query2 = ("SELECT id FROM users WHERE email='$email'");
-$sql = mysql_query($query2) or die(mysql_error());
-if (mysql_num_rows($sql) > 0){
-echo '<font color="red"><img border="0" src="error.gif"  alt="Пользователь с таким e-mail зарегистрированый!">Пользователь с таким e-mail уже зарегистрирован!</font>';
-}
-else{
-$query = "INSERT INTO users (login, password, email, reg_date, name_user, lastname )
-VALUES ('$login', '$mdPassword', '$email', '$rdate', '$name', '$lastname')";
-$result = mysql_query($query) or die(mysql_error());;
-echo '<font color="green"><img border="0" src="ok.gif"  alt="Вы успешно зарегистрировались!">Вы успешно зарегистрировались!</font><br><a href="index.php">На главную</a>';
+$sql_insert ="INSERT INTO registration_on (name, email, date, country) VALUES (?,?,?,?)";
+$stmt = $conn->prepare($sql_insert);
+$stmt->bindValue(1, $name);
+$stmt->bindValue(2, $email);
+$stmt->bindValue(3, $date);
+$stmt->bindValue(4, $country);
+$stmt->execute();
+echo "<h3>Вы зарегистрировались!</h3>";
 }
 }
+catch(Exception $e) {
+die(var_dump($e));
 }
+}
+$sql_select = "SELECT * FROM registration_on";
+$stmt = $conn->query($sql_select);
+$stmt->execute();
+if(isset($_POST['filter'])) {
+$gender = $_POST['country'];
+$sql_select = "SELECT * FROM registration_on WHERE country like :country";
+$stmt = $conn->prepare($sql_select);
+$stmt->execute(array(':country'=>$country.'%'));
+}
+$registrants = $stmt->fetchAll();
+if(count($registrants) > 0) {
+echo "<h2>Люди, которые зарегистрированы:</h2>";
+echo "<table>";
+echo "<tr><th>Name</th>";
+echo "<th>Email</th>";
+echo "<th>Country</th>";
+echo "<th>Date</th></tr>";
+foreach($registrants as $registrant) {
+echo "<td>".$registrant['name']."</td>";
+echo "<td>".$registrant['email']."</td>";
+echo "<td>".$registrant['country']."</td>";
+echo "<td>".$registrant['date']."</td></tr>";
+}
+echo "</table>";
+}
+else {
+echo "<h3>В настоящее время никто не зарегистрирован.</h3>";
 }
 ?>
