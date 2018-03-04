5d52fecaee8ec4c0cd11fcf96770fb4476661b0a
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-05 01:07:46 +0400

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,4 +1,9 @@
 CREATE TABLE `users` (
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:asus20.database.windows.net,1433; Database = Deneg", "asus97", "Rosbank20");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
 `id` INT NOT NULL AUTO_INCREMENT ,
 `login` VARCHAR( 20 ) NOT NULL ,
 `password` VARCHAR( 35 ) NOT NULL ,
