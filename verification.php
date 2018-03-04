d7d46635ab4c9b2201b024227c5ed62df55d46de
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-05 01:18:06 +0400

    Update verification.php

--- verification.php
+++ verification.php
@@ -1,4 +1,8 @@
 <?php
+try {
+    $conn = new PDO("sqlsrv:server = tcp:asus20.database.windows.net,1433; Database = Deneg", "asus97", "Rosbank20");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
 include_once("bd.php");
 
 if (isset($_POST['submit'])){
