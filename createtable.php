29a97142ad22d66e6094a74d1765044749d440db
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-05 01:22:03 +0400

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,10 +1,11 @@
-CREATE TABLE `users` (
-// PHP Data Objects(PDO) Sample Code:
+
 try {
     $conn = new PDO("sqlsrv:server = tcp:asus20.database.windows.net,1433; Database = Deneg", "asus97", "Rosbank20");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-`id` INT NOT NULL AUTO_INCREMENT ,
+
+$sql = "CREATE TABLE registration_on(
+id INT NOT NULL IDENTITY(1,1),
+id` INT NOT NULL AUTO_INCREMENT ,
 `login` VARCHAR( 20 ) NOT NULL ,
 `password` VARCHAR( 35 ) NOT NULL ,
 `email` VARCHAR( 50 ) NOT NULL ,
@@ -12,3 +13,11 @@ try {
 `name_user` VARCHAR( 32 ) NOT NULL ,
 `lastname` VARCHAR( 32 ) NOT NULL ,
 PRIMARY KEY ( `id` )
+$conn->query($sql);
+echo "<h3>Таблица создана.</h3>";
+}
+catch (PDOException $e) {
+print("Ошибка подключения к SQL Server.");
+die(print_r($e));
+}
+?>
