78ec665826c91185edae143f10d22ccd9a3fa207
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-05 02:34:26 +0400

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,37 +1,45 @@
-<table>
-<tr>
-<td>Логин<font color="red">*</font>:</td>
-<td><input type="text" size="20" name="login"></td>
-</tr>
-<tr>
-<td>Пароль<font color="red">*</font>:</td>
-<td><input type="password" size="20" maxlength="20" name="password"></td>
-</tr>
-<tr>
-<td>Подтверждения пароля<font color="red">*</font>:</td>
-<td><input type="password" size="20" maxlength="20" name="password2"></td>
-</tr>
-<tr>
-<td>E-mail<font color="red">*</font>:</td>
-<td><input type="text" size="20" name="email"></td>
-</tr>
-<tr>
-<td>Имя:</td>
-<td><input type="text" size="20" name="name"></td>
-</tr>
-<tr>
-<td>Фамилия:</td>
-<td><input type="text" size="20" name="lastname"></td>
-</tr>
-<tr>
-<td></td>
-<td colspan="2"><input type="submit" value="Зарегистроваться" name="submit">
-</td>
-</tr>
-</table>
+<html>
+<head>
+<Title>Registration Form</Title>
+<style type="text/css">
+body { background-color:
+#fff; border-top: solid 10px #000;
+color: #333; font-size: .85em;
+margin: 20; padding: 20;
+font-family: "Segoe UI",
+Verdana, Helvetica, Sans-Serif;
+}
+h1, h2, h3,{ color: #000;
+margin-bottom: 0; padding-bottom: 0; }
+h1 { font-size: 2em; }
+h2 { font-size: 1.75em; }
+h3 { font-size: 1.2em; }
+table { margin-top: 0.75em; }
+th { font-size: 1.2em;
+text-align: left; border: none; padding-left: 0; }
+td { padding: 0.25em 2em 0.25em 0em;
+border: 0 none; }
+</style>
+</head>
+<body>
+<h1>Register here!</h1>
+<p>Fill in your name and
+email address, then click <strong>Submit</strong>
+to register.</p>
+<form method="post" action="index.php" enctype="multipart/form-data" >
+<input type ="text" name ="name" id ="name" placeholder ="Введите ваше имя">
+<input type ="text" name ="email" id ="email" placeholder ="Ваш еmail..">
+<select name="country">
+<option value="">All</option>
+<option value="Russia">Russia</option>
+<option value="USA">USA</option>
+<option value="Germany">Germany</option>
+<option value="Japan">Japan</option>
+<option value="China">China</option>
+</select>
+<input type ="submit" name ="submit" value ="Отправить">
+<input type="submit" name="filter" value="Фильтр">
 </form>
-
-<br>Поля со значком <font color="red">*</font> обязательны для заполнения
 <?php
 $dsn = "sqlsrv:server = tcp:rom.database.windows.net,1433; Database = qqq";
 $username = "rom";
