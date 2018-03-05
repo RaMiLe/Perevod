c291da9d755454189330303890190a02cb32e1cb
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-05 18:04:40 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -42,10 +42,6 @@ to register.</p>
 </form>
 
 <?php
-$dsn = "sqlsrv:server = tcp:rom.database.windows.net,1433; Database = qqq";
-$username = "rom";
-$password = "Rosbank1997";
-
 $dsn = "sqlsrv:server = tcp:asus20.database.windows.net,1433; Database = deneg";
 $username = "asus97";
 $password = "Rosbank20";
@@ -53,14 +49,16 @@ try {
 $conn = new PDO($dsn, $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-
+catch (PDOException $e) {
+print("Ошибка подключения к SQL Server.");
+die(print_r($e));
+}
 if(!empty($_POST)) {
 try {
 $name = $_POST['name'];
 $email = $_POST['email'];
 $date = date("Y-m-d");
 $country = $_POST['country'];
-
 if ($name == "" || $email == "") {
 echo "<h3>Не заполнены поля name и email.</h3>";
 }
@@ -72,7 +70,6 @@ $stmt->bindValue(2, $email);
 $stmt->bindValue(3, $date);
 $stmt->bindValue(4, $country);
 $stmt->execute();
-
 echo "<h3>Вы зарегистрировались!</h3>";
 }
 }
@@ -80,7 +77,6 @@ catch(Exception $e) {
 die(var_dump($e));
 }
 }
-
 $sql_select = "SELECT * FROM registration_on";
 $stmt = $conn->query($sql_select);
 $stmt->execute();
@@ -109,7 +105,4 @@ echo "</table>";
 else {
 echo "<h3>В настоящее время никто не зарегистрирован.</h3>";
 }
-
 ?>
-
-
