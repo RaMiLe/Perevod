e62a4c0cf1cb63f3d326378b9f4d8c794e0e03ab
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-07 20:13:26 +0300

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,12 +1,11 @@
+<?php
+session_start();
+
 mysql_connect ("asus20","Deneg","Rosbank20");
 mysql_select_db ("asus97");
-CREATE TABLE `users` (
-`id` INT NOT NULL AUTO_INCREMENT ,
-`login` VARCHAR( 20 ) NOT NULL ,
-`password` VARCHAR( 35 ) NOT NULL ,
-`email` VARCHAR( 50 ) NOT NULL ,
-`reg_date` VARCHAR( 32 ) NOT NULL ,
-`name_user` VARCHAR( 32 ) NOT NULL ,
-`lastname` VARCHAR( 32 ) NOT NULL ,
-PRIMARY KEY ( `id` )
-);
+mysql_query("SET NAMES utf8");
+
+$login = $_SESSION['login'];
+$password = $_SESSION['password'];
+$id_user = $_SESSION['id'];
+?>
