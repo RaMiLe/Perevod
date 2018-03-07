695178ffc33b3370240a0b8116d1a9cc676c14f6
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-07 19:46:13 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -56,57 +56,34 @@ catch (PDOException $e) {
 print("Ошибка подключения к SQL Server.");
 die(print_r($e));
 }
-if(!empty($_POST)) {
-try {
-$name = $_POST['name'];
-$email = $_POST['email'];
-$date = date("Y-m-d");
-$country = $_POST['country'];
-if ($name == "" || $email == "") {
-echo "<h3>Не заполнены поля name и email.</h3>";
-}
-else {
-$sql_insert ="INSERT INTO registration_on (name, email, date, country) VALUES (?,?,?,?)";
-$stmt = $conn->prepare($sql_insert);
-$stmt->bindValue(1, $name);
-$stmt->bindValue(2, $email);
-$stmt->bindValue(3, $date);
-$stmt->bindValue(4, $country);
-$stmt->execute();
-echo "<h3>Вы зарегистрировались!</h3>";
-}
-}
-catch(Exception $e) {
-die(var_dump($e));
-}
-}
-$sql_select = "SELECT * FROM registration_on";
-$stmt = $conn->query($sql_select);
-$stmt->execute();
-if(isset($_POST['filter'])) {
-$gender = $_POST['country'];
-$sql_select = "SELECT * FROM registration_on WHERE country like :country";
-$stmt = $conn->prepare($sql_select);
-$stmt->execute(array(':country'=>$country.'%'));
-}
-$registrants = $stmt->fetchAll();
-if(count($registrants) > 0) {
-echo "<h2>Люди, которые зарегистрированы:</h2>";
-echo "<table>";
-echo "<tr><th>Name</th>";
-echo "<th>Email</th>";
-echo "<th>Country</th>";
-echo "<th>Date</th></tr>";
-foreach($registrants as $registrant) {
-echo "<td>".$registrant['name']."</td>";
-echo "<td>".$registrant['email']."</td>";
-echo "<td>".$registrant['country']."</td>";
-echo "<td>".$registrant['date']."</td></tr>";
-}
-echo "</table>";
-}
-else {
-echo "<h3>В настоящее время никто не зарегистрирован.</h3>";
-}
+
+    $username = $_POST['username'];
+    $password_1 = $_POST['password-1'];
+    $password_2 = $_POST['password-2'];
+    $email = $_POST['email'];
+
+    if (!empty($username) && !empty($password_1) && !empty($password_2) && ($password_1 == $password_2)) {
+      $sql_select = "SELECT * FROM signup WHERE username = '$username'";
+      $stmt = $conn->query($sql_select);
+      $stmt->execute();
+      $data = $stmt->fetchAll();
+
+      if(count($data) == 0) {
+        $sql_insert = "INSERT INTO signup (username, password, email) VALUES (?,?,?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $username);
+        $stmt->bindValue(2, $password_1);
+        $stmt->bindValue(3, $email);
+        $stmt->execute();
+        
+        echo 'Вы зарегистрированны!';
+        exit();
+      }
+      else {
+        echo 'Такой пользователь уже существует!';
+      }
+    }
+  }
 ?>
+
 
