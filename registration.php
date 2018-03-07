adf82b93fcd9e049267f2a45bf35f3b8da8976c7
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-07 19:23:09 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,58 +1,109 @@
+<html>
+<head>
+<Title>Registration Form</Title>
+<style type="text/css">
+body { background-color:
+#fff; border-top: solid 10px #000;
+color: #333; font-size: .85em;
+margin: 20; padding: 20;
+font-family: "Segoe UI",
+Verdana, Helvetica, Sans-Serif;
+}
+h1, h2, h3,{ color: #000;
+margin-bottom: 0; padding-bottom: 0; }
+h1 { font-size: 2em; }
+h2 { font-size: 1.75em; }
+h3 { font-size: 1.2em; }
+table { margin-top: 0.75em; }
+th { font-size: 1.2em;
+text-align: left; border: none; padding-left: 0; }
+td { padding: 0.25em 2em 0.25em 0em;
+border: 0 none; }
+</style>
+</head>
+<body>
+<h1>Register here!</h1>
+<p>Fill in your name and
+email address, then click <strong>Submit</strong>
+to register.</p>
+<form method="post" action="index.php" enctype="multipart/form-data" >
+<input type ="text" name ="name" id ="name" placeholder ="Введите ваше имя">
+<input type ="text" name ="email" id ="email" placeholder ="Ваш еmail..">
+<select name="country">
+<option value="">All</option>
+<option value="Russia">Russia</option>
+<option value="USA">USA</option>
+<option value="Germany">Germany</option>
+<option value="Japan">Japan</option>
+<option value="China">China</option>
+</select>
+<input type ="submit" name ="submit" value ="Отправить">
+<input type="submit" name="filter" value="Фильтр">
+</form>
+
 <?php
-
-require_once('createtable.php');
-
-if (isset($_POST['submit'])) {
-
-    $username = $_POST['username'];
-    $password_1 = $_POST['password-1'];
-    $password_2 = $_POST['password-2'];
-    $email = $_POST['email'];
-
-    if (!empty($username) && !empty($password_1) && !empty($password_2) && ($password_1 == $password_2)) {
-      $sql_select = "SELECT * FROM signup WHERE username = '$username'";
-      $stmt = $conn->query($sql_select);
-      $stmt->execute();
-      $data = $stmt->fetchAll();
-
-      if(count($data) == 0) {
-        $sql_insert = "INSERT INTO signup (username, password, email) VALUES (?,?,?)";
-        $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $username);
-        $stmt->bindValue(2, $password_1);
-        $stmt->bindValue(3, $email);
-        $stmt->execute();
-        
-        echo 'Вы зарегистрированны!';
-        exit();
-      }
-      else {
-        echo 'Такой пользователь уже существует!';
-      }
-    }
-  }
+$dsn = "sqlsrv:server = tcp:rom.database.windows.net,1433; Database = qqq";
+$username = "rom";
+$password = "Rosbank1997";
+try {
+$conn = new PDO($dsn, $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+print("Ошибка подключения к SQL Server.");
+die(print_r($e));
+}
+if(!empty($_POST)) {
+try {
+$name = $_POST['name'];
+$email = $_POST['email'];
+$date = date("Y-m-d");
+$country = $_POST['country'];
+if ($name == "" || $email == "") {
+echo "<h3>Не заполнены поля name и email.</h3>";
+}
+else {
+$sql_insert ="INSERT INTO registration_on (name, email, date, country) VALUES (?,?,?,?)";
+$stmt = $conn->prepare($sql_insert);
+$stmt->bindValue(1, $name);
+$stmt->bindValue(2, $email);
+$stmt->bindValue(3, $date);
+$stmt->bindValue(4, $country);
+$stmt->execute();
+echo "<h3>Вы зарегистрировались!</h3>";
+}
+}
+catch(Exception $e) {
+die(var_dump($e));
+}
+}
+$sql_select = "SELECT * FROM registration_on";
+$stmt = $conn->query($sql_select);
+$stmt->execute();
+if(isset($_POST['filter'])) {
+$gender = $_POST['country'];
+$sql_select = "SELECT * FROM registration_on WHERE country like :country";
+$stmt = $conn->prepare($sql_select);
+$stmt->execute(array(':country'=>$country.'%'));
+}
+$registrants = $stmt->fetchAll();
+if(count($registrants) > 0) {
+echo "<h2>Люди, которые зарегистрированы:</h2>";
+echo "<table>";
+echo "<tr><th>Name</th>";
+echo "<th>Email</th>";
+echo "<th>Country</th>";
+echo "<th>Date</th></tr>";
+foreach($registrants as $registrant) {
+echo "<td>".$registrant['name']."</td>";
+echo "<td>".$registrant['email']."</td>";
+echo "<td>".$registrant['country']."</td>";
+echo "<td>".$registrant['date']."</td></tr>";
+}
+echo "</table>";
+}
+else {
+echo "<h3>В настоящее время никто не зарегистрирован.</h3>";
+}
 ?>
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="/css/sug.css">
-    <link rel="stylesheet" href="/css/font-awesome.css">
-    <title>Страхование жизни</title>
-  </head>
-  <body>
-    <div class="container">
-      <a href="/index.php"><img src="img/lock.png"></a>
-      <form class="" action="sugnup.php" method="post">
-        <div class="dws-input">
-          <input type="text" name="username" placeholder="Придумайте логин">
-          <input type="password" name="password-1" placeholder="Придумайте пароль">
-          <input type="password" name="password-2" placeholder="Введите пароль еще раз">
-          <input type="text" name="email" placeholder="Ваш email...">
-        </div>
-        <input class="dws-submit" type="submit" name="submit" value="Регистрация">
-      </form>
-    </div>
-  </body>
-</html>
