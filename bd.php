82da3adeca3654180ce75c8930b2dd75adca5556
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-08 10:58:24 +0300

    Update bd.php

--- bd.php
+++ bd.php
@@ -9,14 +9,17 @@ $login = $_SESSION['login'];
 $password = $_SESSION['password'];
 $id_user = $_SESSION['id'];
 ;
+CREATE TABLE  `users` (
+ `id` INT NOT NULL AUTO_INCREMENT ,
+ `login` VARCHAR( 20 ) NOT NULL ,
+ `password` VARCHAR( 35 ) NOT NULL ,
+ `email` VARCHAR( 50 ) NOT NULL ,
+ `reg_date` VARCHAR( 32 ) NOT NULL ,
+ `name_user` VARCHAR( 32 ) NOT NULL ,
+ `lastname` VARCHAR( 32 ) NOT NULL ,
+PRIMARY KEY (  `id` )
+);
+
+
 ?>
-CREATE TABLE `users` (
-`id` INT NOT NULL AUTO_INCREMENT ,
-`login` VARCHAR( 20 ) NOT NULL ,
-`password` VARCHAR( 35 ) NOT NULL ,
-`email` VARCHAR( 50 ) NOT NULL ,
-`reg_date` VARCHAR( 32 ) NOT NULL ,
-`name_user` VARCHAR( 32 ) NOT NULL ,
-`lastname` VARCHAR( 32 ) NOT NULL ,
-PRIMARY KEY ( `id` )
-)
+
